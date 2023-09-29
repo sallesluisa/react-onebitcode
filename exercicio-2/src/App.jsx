246ead9de7b58aa29897b61c7fc4db90a424055f
Luísa Salles <luisacarvalhosalles@gmail.com>
2023-09-29 14:59:40 -0300

    Criando um Componente Card

---
+++ exercicio-2/src/App.jsx
@@ -0,0 +1,9 @@
+import Card from "./Card"
+
+export default function App() {
+  return (
+    <div className="app">
+      <Card />
+    </div>
+  )
+}
---
+++ exercicio-2/src/Card.jsx
@@ -0,0 +1,19 @@
+export default function Card() {
+  return (
+    <div className='container'>
+      <img src='./poster_starwars.jpg' alt='Poster Star Wars' />
+
+      <div className='info'>
+        <h1>Pôster: Star Wars (1977)</h1>
+        <p>
+          Um pôster decorativo épico do filme Star Wars, com moldura de MDF e
+          tamanho АЗ. Uma ótima recordação de um dos mais icônicos filmes de
+          todos os tempos. Este clássico pôster trará aventura, nostalgia e a
+          magia de Star Wars para qualquer lugar que você decidir pendurar. Não
+          perca a chance de adicionar essa linda memória ao seu acervo!
+        </p>
+        <button>Comprar agora</button>
+      </div>
+    </div>
+  );
+}
